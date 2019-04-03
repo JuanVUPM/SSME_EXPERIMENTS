e9d4aa1624cb6a22bad4e4de0335b71d26806d21
alumnosLPRES <alumnos.upm.lpres@gmail.com>
2019-04-03 15:02:40 +0200

    First commit SSME experiments
    Signed-off-by: alumnosLPRES <alumnos.upm.lpres@gmail.com>

---
+++ sources/version.el
@@ -0,0 +1,7 @@
+-- Define the version, date, author and comments associated to this library
+#LIBRARY_INFO
+	#VERSION = "0.0"
+	#DATE = "01/03/2019"
+	#AUTHOR = ""
+	#COMMENTS= "No comments"
+END #LIBRARY_INFO
